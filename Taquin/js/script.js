9d3f5dd3c41510ab57ba69ddce6cf7bbc5f232e7
Crespin Alexandre <126071849+CRESPIN-Alexandre-2225022aa@users.noreply.github.com>
2024-01-31 18:08:40 +0100

    finish : Taquin

---
+++ Taquin/js/script.js
@@ -0,0 +1,14 @@
+let ligneVide = 4;
+let columnVide = 4;
+
+function move(ligne,column) {
+    if (((ligneVide - ligne == 1 || ligneVide - ligne == -1) && columnVide == column) || ((columnVide - column == 1 || columnVide - column == -1) && ligneVide == ligne)) {
+        let caseVide = document.getElementById(ligneVide + "" + columnVide);
+        caseVide.type="button";
+        let value = document.getElementById(ligne + "" + column).value;
+        caseVide.value=value;
+        ligneVide = ligne;
+        columnVide = column;
+        document.getElementById(ligneVide + "" + columnVide).type="hidden";
+    }
+}
